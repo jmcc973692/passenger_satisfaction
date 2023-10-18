52ecb6cf0c5fa37ff400b1a7bab808601f337513
Jay McCreary <d1125@davidson-tech-sf.com>
2023-10-17 21:06:09 -0500

    Best NN Results so far - 0.96552 leaderboard accuracy

--- neural_network.py
+++ neural_network.py
@@ -1,10 +1,23 @@
+import os
+from datetime import datetime
+
 import torch
 from sklearn.model_selection import train_test_split
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader, TensorDataset
 
 from src.data_handling import prepare_data
 from src.feature_engineering import perform_feature_engineering_nn
+from src.submission import load_sample_submission, save_submission, update_submission_structure
 from src.TabularNN import TabularNN
+
+
+def compute_accuracy(predictions, labels):
+    # Convert sigmoid outputs to binary predictions
+    predicted_labels = (predictions > 0.5).float()
+    correct = (predicted_labels == labels).float().sum()
+    accuracy = correct / len(labels)
+    return accuracy.item()
 
 
 def main(train_path, test_path, sample_submission_path, submission_dir):
@@ -16,11 +29,17 @@ def main(train_path, test_path, sample_submission_path, submission_dir):
 
     train_x = train_df.drop(columns=["Satisfaction_Rating", "id"])
     train_y = train_df["Satisfaction_Rating"]
+    test_df = test_df.drop(columns=["id"])
 
-    print(train_x.isnull().sum())
+    # Ensure both dataframes have the same column order
+    train_x = train_x[sorted(train_x.columns)]
+    test_df = test_df[sorted(test_df.columns)]
+
     # Convert data to PyTorch tensors
     train_x_tensor = torch.FloatTensor(train_x.values)
     train_y_tensor = torch.FloatTensor(train_y.values).unsqueeze(1)  # Convert to 2D tensor
+    # Convert test data to tensor
+    test_x_tensor = torch.FloatTensor(test_df.values)  # Assuming 'id' column is present in test_df
 
     # Split data
     x_train, x_val, y_train, y_val = train_test_split(train_x_tensor, train_y_tensor, test_size=0.2, random_state=42)
@@ -39,12 +58,19 @@ def main(train_path, test_path, sample_submission_path, submission_dir):
 
     # Hyperparameters
     learning_rate = 0.001
-    epochs = 20
+    weight_decay = 0
+
+    # Early stopping parameters
+    patience = 10  # number of epochs to wait for improvement before terminating
+    best_val_loss = float("inf")
+    epochs_no_improve = 0
 
     # Loss and optimizer
     criterion = torch.nn.BCELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    scheduler = ReduceLROnPlateau(optimizer, mode="min", factor=0.1, patience=5, verbose=True)
 
+    epochs = 1000
     # Training loop
     for epoch in range(epochs):
         model.train()
@@ -60,7 +86,7 @@ def main(train_path, test_path, sample_submission_path, submission_dir):
 
             running_loss += loss.item()
 
-        print(f"Epoch [{epoch+1}/{epochs}], Training Loss: {1 - running_loss/len(train_loader):.4f}")
+        print(f"Epoch [{epoch+1}/{epochs}], Training Loss: {running_loss/len(train_loader):.4f}")
 
         # Validation loop
         model.eval()
@@ -71,10 +97,51 @@ def main(train_path, test_path, sample_submission_path, submission_dir):
                 loss = criterion(outputs, batch_labels)
                 val_loss += loss.item()
 
-        print(f"Epoch [{epoch+1}/{epochs}], Validation Loss: {1 - val_loss/len(val_loader):.4f}")
+        print(f"Epoch [{epoch+1}/{epochs}], Validation Loss: {val_loss/len(val_loader):.4f}")
+
+        # Step the Scheduler
+        scheduler.step(val_loss)
+
+        # Check if the validation loss improved
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            epochs_no_improve = 0  # Reset the Counter
+        else:
+            epochs_no_improve += 1
+
+        if epochs_no_improve == patience:
+            print("Early Stopping!")
+            break
+
+    with torch.no_grad():
+        model.eval()
+
+        train_predictions = model(train_x_tensor)
+        train_accuracy = compute_accuracy(train_predictions, train_y_tensor)
+        print(f"Final Training Accuracy: {train_accuracy:.4f}")
 
     # Save model
     torch.save(model.state_dict(), "./models/nn_model.pt")
+
+    # Load sample submission
+    sample_submission_df = load_sample_submission(sample_submission_path)
+
+    # Make predictions on the test set
+    model.eval()
+    with torch.no_grad():
+        test_outputs = model(test_x_tensor)
+        # Convert probabilities to binary labels; using 0.5 as threshold
+        y_pred_test = (test_outputs > 0.5).float().numpy().flatten()
+
+    # Create Submission
+    timestamp = datetime.now().strftime("%m_%d_%Y_%H-%M")
+    submission_file_name = f"nn_submission_{timestamp}.csv"
+    submission_path = os.path.join(submission_dir, submission_file_name)
+    # Update submission structure
+    update_submission_structure(sample_submission_df, y_pred_test)
+    save_submission(sample_submission_df, submission_path)
+
+    print(f"Submission saved to {submission_path}")
 
 
 if __name__ == "__main__":
--- src/TabularNN.py
+++ src/TabularNN.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class TabularNN(nn.Module):
@@ -6,22 +7,70 @@ class TabularNN(nn.Module):
         super(TabularNN, self).__init__()
 
         # Define layers
-        self.layer1 = nn.Linear(input_dim, 128)
-        self.layer2 = nn.Linear(128, 64)
-        self.layer3 = nn.Linear(64, 32)
-        self.output_layer = nn.Linear(32, 1)  # Binary classification -> single output neuron
+        self.layer1 = nn.Linear(input_dim, 512)
+        self.bn1 = nn.BatchNorm1d(512)
+
+        self.layer2 = nn.Linear(512, 256)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.res1 = nn.Linear(512, 256)  # Residual connection for layer2
+
+        self.layer3 = nn.Linear(256, 256)
+        self.bn3 = nn.BatchNorm1d(256)
+
+        self.layer4 = nn.Linear(256, 128)
+        self.bn4 = nn.BatchNorm1d(128)
+        self.res2 = nn.Linear(256, 128)  # Residual connection for layer4
+
+        self.layer5 = nn.Linear(128, 128)
+        self.bn5 = nn.BatchNorm1d(128)
+
+        self.layer6 = nn.Linear(128, 64)
+        self.bn6 = nn.BatchNorm1d(64)
+        self.res3 = nn.Linear(128, 64)  # Residual connection for layer6
+
+        self.output_layer = nn.Linear(64, 1)
 
         # Activation and dropout
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.2)
+        self.dropout = nn.Dropout(0.3)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        x = self.relu(self.layer1(x))
+        x = self.layer1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
         x = self.dropout(x)
-        x = self.relu(self.layer2(x))
+
+        # Store the output for residual connection
+        res1_output = x
+        x = self.layer2(x)
+        x = self.bn2(x)
+        x = self.relu(x + self.res1(res1_output))
         x = self.dropout(x)
-        x = self.relu(self.layer3(x))
+
+        x = self.layer3(x)
+        x = self.bn3(x)
+        x = self.relu(x)
         x = self.dropout(x)
+
+        # Store the output for residual connection
+        res2_output = x
+        x = self.layer4(x)
+        x = self.bn4(x)
+        x = self.relu(x + self.res2(res2_output))
+        x = self.dropout(x)
+
+        x = self.layer5(x)
+        x = self.bn5(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+
+        # Store the output for residual connection
+        res3_output = x
+        x = self.layer6(x)
+        x = self.bn6(x)
+        x = self.relu(x + self.res3(res3_output))
+        x = self.dropout(x)
+
         x = self.sigmoid(self.output_layer(x))
         return x

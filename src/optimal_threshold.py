84e9d2e04d831fb4609ccfe749017d1fbd112309
Jay McCreary <d1125@davidson-tech-sf.com>
2023-11-06 21:24:04 -0600

    New optimal_threshold module

---
+++ src/optimal_threshold.py
@@ -0,0 +1,42 @@
+from functools import partial, update_wrapper
+
+from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
+from sklearn.metrics import accuracy_score, make_scorer
+from sklearn.model_selection import cross_val_score
+
+
+def custom_accuracy_score(y_true, y_pred_probs, threshold):
+    y_pred = (y_pred_probs >= threshold).astype(int)
+    return accuracy_score(y_true, y_pred)
+
+
+def threshold_scorer(threshold):
+    # Define a custom scorer that uses the threshold
+    def score_func(y_true, y_pred_probs):
+        return custom_accuracy_score(y_true, y_pred_probs, threshold)
+
+    # Set the __name__ attribute to avoid the AttributeError
+    score_func.__name__ = f"custom_accuracy_score_{threshold}"
+    return make_scorer(score_func, needs_proba=True)
+
+
+def threshold_objective(threshold, X, y, model):
+    # Get the custom scorer with the current threshold
+    scorer = threshold_scorer(threshold)
+
+    # Calculate cross-validation score using the scorer
+    scores = cross_val_score(model, X, y, cv=10, scoring=scorer, n_jobs=-1)
+
+    mean_cv_score = scores.mean()
+    return {"loss": -mean_cv_score, "status": STATUS_OK}
+
+
+def find_optimal_threshold(X, y, model):
+    space = hp.uniform("threshold", 0.25, 0.75)
+
+    trials = Trials()
+    objective = partial(threshold_objective, X=X, y=y, model=model)
+    optimal_threshold = fmin(fn=objective, space=space, algo=tpe.suggest, max_evals=75, trials=trials)
+
+    print(f"Optimal Threshold = {optimal_threshold['threshold']}")
+    return optimal_threshold["threshold"]
